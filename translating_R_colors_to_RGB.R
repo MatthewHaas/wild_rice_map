c036f182fb1a74b714e55c1ce1d95835d2138564
Matthew Haas <matthew.haas1@gmail.com>
2020-02-22 11:01:57 -0600

    Initial commit
    R code for translating R color name to RGB values--this should make it easy to replicate R colors with Python/geopandas.

---
+++ translating_R_colors_to_RGB.R
@@ -0,0 +1,18 @@
+# 22 February 2020
+# R code for translating color names to red, green, blue (RGB) values
+# It should be possible to specify these RGB values withy Python/geopandas to recreate the R colors.
+
+col2rgb("red3") # (205,0,0) (Aquatica)
+col2rgb("red") # (255,0,0) (Bass Lake)
+col2rgb("orange3") # (205,133,0) (Big Fork River)
+col2rgb("orange") # (255,165,0) (Clearwater River)
+col2rgb("yellow3") # (205,205,0) (Dahler Lake)
+col2rgb("yellow") # (255,255,0) (Decker Lake)
+col2rgb("green3") # (0,205,0) (Garfield Lake)
+col2rgb("green") # (0,255,0) (Mud Hen Lake)
+col2rgb("blue4") # (0,0,139) (Necktie River)
+col2rgb("blue") # (0,0,255) (Ottertail River)
+col2rgb("violetred3") # (205,50,120) (Phantom Lake)
+col2rgb("violet") # (238,130,238) (Plantagenet)
+col2rgb("purple4") # (85,26,139) (Shell Lake)
+col2rgb("purple") # (160,32,240) (Upper Rice Lake)
